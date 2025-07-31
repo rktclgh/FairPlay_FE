6e79e77d7012789ae409f8292f7a94158830a2cf
takonism <takonism@naver.com>
2025-07-31 23:49:28 +0900

    feat: FindPassword 추가 및 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Main } from './pages/MainPage'
 import { LoginPage } from './pages/LoginPage'
 import { SignUpPage } from './pages/SignUpPage'
+import { FindPassword } from './pages/FindPassword'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
@@ -13,7 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/find-password" element={<FindPassword />} />
         </Routes>
       </Router>
       {/* ToastContainer는 무조건 최상단에, 한 번만 */}
