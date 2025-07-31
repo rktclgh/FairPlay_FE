199e4cf854cdd4fdf448851e7b4e4146eaa8b097
rktclgh <songchih@icloud.com>
2025-07-31 20:34:07 +0900

    설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Main } from './pages/MainPage'
 import { LoginPage } from './pages/LoginPage'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Routes>
+            </Router>
+            {/* ToastContainer는 무조건 최상단에, 한 번만 */}
+            <ToastContainer position="top-center" autoClose={1800} />
+        </>
+    )
 }
 
 export default App
