73cabb787335a61c9daa719db00b8c68f01aa4a4
rktclgh <songchih@icloud.com>
2025-08-05 02:14:48 +0900

    배포테스트용 axios 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 
 const api = axios.create({
     baseURL: 'http://localhost:8080',
+    // baseURL: 'https://fair-play.ink',
     withCredentials: true,
 });
 
