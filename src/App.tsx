6b0a15f63382361ec5fb08edff7a29eea9abc2fd
takonism <takonism@naver.com>
2025-08-08 15:57:20 +0900

    feat: 예약자 명단

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import { EditEventInfo } from "./pages/host_event/EditEventInfo";
 import TicketManagement from "./pages/host_event/TicketManagement";
 import RoundManagement from "./pages/host_event/RoundManagement";
 import { EventStatusBanner } from "./pages/host_event/EventStatusBanner";
+import { ReservationList } from "./pages/host_event/ReservationList";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -58,6 +59,7 @@ function AppContent() {
         <Route path="/host/ticket-management" element={<TicketManagement />} />
         <Route path="/host/round-management" element={<RoundManagement />} />
         <Route path="/host/status-management" element={<EventStatusBanner />} />
+        <Route path="/host/reservation-list" element={<ReservationList />} />
         <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
       </Routes>
       <ToastContainer
