2d8feb3e5147b21e00917c51d929b29c26e0d9c3
Happy-Lotus <happylotus145@gmail.com>
2025-08-12 01:12:53 +0900

    feat: 참석자 및 QR 티켓 API 설정

---
+++ src/services/qrTicket.ts
@@ -0,0 +1,93 @@
+import api from "@/api/axios";
+import type {
+    QrTicketRequestDto,
+    QrTicketResponseDto,
+    QrTicketUpdateRequestDto,
+    QrTicketUpdateResponseDto,
+    QrTicketReissueRequestDto,
+    QrTicketReissueResponseDto,
+    MemberQrCheckRequestDto,
+    MemberManualCheckRequestDto,
+    GuestQrCheckRequestDto,
+    GuestManualCheckRequestDto,
+    CheckResponseDto,
+    AdminForceCheckRequestDto
+} from "./types/qrTicketType";
+
+// 마이페이지에서 QR 티켓 조회
+export const getQrTicketForMypage = async (data: QrTicketRequestDto): Promise<QrTicketResponseDto> => {
+    const res = await api.post<QrTicketResponseDto>(`/api/qr-tickets`,data);
+    return res.data;
+}
+
+// 비회원 QR 티켓 링크를 통한 조회
+export const getQrTicketForLink = async (token: string): Promise<QrTicketResponseDto> => {
+    const res = await api.get<QrTicketResponseDto>(`/api/qr-tickets/${token}`);
+    return res.data;
+}
+
+// QR 티켓 화면에서 새로고침 버튼 이용해 재발급 
+export const reissueQrTicket = async (data: QrTicketUpdateRequestDto): Promise<QrTicketUpdateResponseDto> => {
+    const res = await api.post<QrTicketUpdateResponseDto>(`/api/qr-tickets/reissue`,data);
+    return res.data;
+}
+
+// 회원이 마이페이지에서 QR 링크 조회 안될 때 관리자 강제 QR 티켓 리셋 요청
+export const reissueAdminQrTicketByUser = async (data: QrTicketReissueRequestDto): Promise<QrTicketReissueResponseDto> => {
+    const res = await api.post<QrTicketReissueResponseDto>(`/api/qr-tickets/admin/reissue`,data);
+    return res.data;
+}
+
+// 회원, 비회원 관리자 강제 QR 티켓 리셋 후 재발급 이메일 발송
+export const reissueAdminQrTicket = async (data: QrTicketReissueRequestDto): Promise<QrTicketReissueResponseDto> => {
+    const res = await api.post<QrTicketReissueResponseDto>(`/api/qr-tickets/admin/reissue/send-email`,data);
+    return res.data;
+}
+
+// 회원 + QR 체크인
+export const checkInWithQrByMember = async (data: MemberQrCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-in/member/qr`,data);
+    return res.data;
+}
+
+// 회원 + 수동코드 체크인
+export const checkInWithManualByMember = async (data: MemberManualCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-in/member/manual`,data);
+    return res.data;
+}
+
+// 비회원 + QR 체크인
+export const checkInWithQrByGuest = async (data: GuestQrCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-in/guest/q`,data);
+    return res.data;
+}
+
+// 회원 + QR 체크아웃
+export const checkOutWithQrByMember = async (data: GuestManualCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-out/guest/manual`,data);
+    return res.data;
+}
+
+// 회원 + 수동코드 체크아웃
+export const checkOutWithManualByMember = async (data: MemberQrCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-out/guest/manual`,data);
+    return res.data;
+}
+
+// 비회원 + QR 체크아웃
+export const checkOutWithQrByGuest = async (data: MemberManualCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-out/guest/manual`,data);
+    return res.data;
+}
+
+// 비회원 + 수동코드 체크아웃
+export const checkOutWithManualByGuest = async (data: GuestQrCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/check-out/guest/manual`,data);
+    return res.data;
+}
+
+// 관리자 강제 체크인/체크아웃
+export const adminForceCheck = async (data: AdminForceCheckRequestDto): Promise<CheckResponseDto> => {
+    const res = await api.post<CheckResponseDto>(`/api/qr-tickets/admin/check`,data);
+    return res.data;
+}
---
+++ src/services/types/qrTicketType.ts
@@ -0,0 +1,84 @@
+// 마이페이지 QR 티켓 조회 요청
+export interface QrTicketRequestDto{
+    attendeeId: number,
+    eventId: number,
+    ticketId: number,
+    reservationId: number
+}
+
+interface ViewingScheduleInfo{
+        date: string, 
+    dayOfWeek: string, 
+    startTime: string; 
+}
+
+// 마이페이지 QR 티켓 조회 또는 비회원 QR 티켓 조회 응답
+export interface QrTicketResponseDto{
+    title: string, // 행사 제목
+    buildingName: string, // 행사 장소
+    qrCode: string, // QR 이미지 코드
+    manualCode: string, // 수동 코드
+    ticketNo: string, // 티켓 번호
+    viewingScheduleInfo: ViewingScheduleInfo, // 관람 일시 정보
+    reservationDate: string // 예매일
+}
+
+// QR 화면 새로고침 버튼 이용한 재발급 요청 
+export interface QrTicketUpdateRequestDto{
+    qrUrlToken: string // qr 화면 url 맨 뒤 token 값
+}
+
+// QR 화면 새로고침 버튼 이용한 재발급 응답 
+export interface QrTicketUpdateResponseDto{
+    qrCode: string, // 이미지 코드
+    manualCode: string // 수동코드
+}
+
+//  회원이 마이페이지에서 QR 링크 조회 안될 때 관리자 강제 QR 티켓 리셋 요청
+// 회원/비회원 QR 티켓 리셋 요청 후 이메일로 재발급 링크 발급 요청
+export interface QrTicketReissueRequestDto{
+    attendeeId: number, // 참석자 ID
+    ticketNo: string // 티켓 번호
+}
+
+// QR 티켓 재발급 응답 
+export interface QrTicketReissueResponseDto{
+    ticketNo: string, // 티켓 번호
+    email: string
+}
+
+// 회원 + QR 체크인/체크아웃
+export interface MemberQrCheckRequestDto{
+    reservationId: number,
+    qrCode: string
+}
+
+// 회원 + 수동코드 체크인/체크아웃
+export interface MemberManualCheckRequestDto{
+    reservationId: number,
+    manualCode: string
+}
+
+// 비회원 + QR 체크인/체크아웃
+export interface GuestQrCheckRequestDto{
+    qrLinkToken: string,
+    qrCode: string    
+}
+
+// 비회원 + 수동코드 체크인/체크아웃
+export interface GuestManualCheckRequestDto{
+    qrLinkToken: string,
+    manualCode: string    
+}
+
+// 체크인/체크아웃 응답
+export interface CheckResponseDto{
+    message: string,
+    checkInTime: Date    
+}
+
+// 관리자 강제 체크인/체크아웃 요청
+export interface AdminForceCheckRequestDto{
+    ticketNo: string,
+    qrCheckStatusCode: string
+}
